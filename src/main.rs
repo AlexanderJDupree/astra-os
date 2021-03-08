5155e32bf68cd6cf0aab01a29dd141331e887b72
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-03-07 21:16:38 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+//  main.rs
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+static HELLO: &[u8] = b"Hello World!";
+
+// Ensures rustc doesn't generate a hashed fn name
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb80000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+
+    loop {}
+}
